0b7677f1533cbf5e3fee6bb582928f44c2b88e57
filou59 <p.git@flot.ovh>
2021-03-06 10:46:27 +0100

    Update Upd_pve_repo_nag.sh

--- Upd_pve_repo_nag.sh
+++ Upd_pve_repo_nag.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
 
-# Utilisation :
+# Filou59 - 2021
+
+# Script pour Retirer NAG et Fix Repo
+
+# USAGE
+# You can run this scritp directly using:
 # wget -q -O - https://raw.githubusercontent.com/filou59/Pve/main/Upd_pve_repo_nag.sh | bash
 
+varversion=1.0
+# V1.0: Initial Release with support for both PVE and PBS
+
 echo "----------------------------------------------------------------"
-echo "Tonton Jo - 2021"
+echo "Filou59 - 2021"
 echo "Proxmox subscription and sources inital setup V$varversion"
 echo "----------------------------------------------------------------"
 exit 0
+# -----------------ENVIRONNEMENT VARIABLES----------------------
+
 # Disable Commercial Repo
-sed -i "s/^deb/\#deb/" /etc/apt/sources.list.d/pve-enterprise.list && apt-get update
+sed -i "s/^deb/\#deb/" /etc/apt/sources.list.d/pve-enterprise.list 
+apt-get update
 
 # Add PVE Community Repo
 echo "deb http://download.proxmox.com/debian/pve $(grep "VERSION=" /etc/os-release | sed -n 's/.*(\(.*\)).*/\1/p') pve-no-subscription" > /etc/apt/sources.list.d/pve-no-enterprise.list && apt-get update
